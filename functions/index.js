d2336079e82d48dfb361f554ccecb5d3d32ca70c
Scott Mueller <scott@ucode.com>
2019-02-03 17:39:55 -0800

    Automatically updating and deleting Angolia decks index

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,25 @@
 const functions = require('firebase-functions')
-
-const ALGOLIA_ID = functions.config().algolia.app_id
-const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
-const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key
+const algoliasearch = require('algoliasearch')
+const env = functions.config()
+const ALGOLIA_ID = env.algolia.app_id
+const ALGOLIA_ADMIN_KEY = env.algolia.api_key
+//const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key
 
 const ALGOLIA_INDEX_NAME = 'decks'
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
+const index = client.initIndex(ALGOLIA_INDEX_NAME)
 
-exports.deckCreated = functions.firestore.document('decks/{deckId}').onCreate((snapshot, context) => {
-    const deck = snapshot.data()
-    deck.objectID = context.params.deckId
-    const index = client.initIndex(ALGOLIA_INDEX_NAME)
-    return index.saveObject(deck)
-})
+const addKeyVal = obj => key => val => {
+	obj[key] = val
+	return obj
+}
+
+const updateDeckInAngolia = (snapshot, context) =>
+    index.saveObject(addKeyVal(snapshot.data())('objectID')(context.params.deckId))
+
+const deleteDeckInAngolia = snapshot =>
+    index.deleteObject(snapshot.id)
+
+exports.deckCreated = functions.firestore.document('decks/{deckId}').onCreate(updateDeckInAngolia)
+exports.deckUpdated = functions.firestore.document('decks/{deckId}').onUpdate(updateDeckInAngolia)
+exports.deckDeleted = functions.firestore.document('decks/{deckId}').onDelete(deleteDeckInAngolia)
