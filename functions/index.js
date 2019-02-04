6fefb28902015bc3caf8e4ffb9bcb088cf45ae4a
Scott Mueller <scott@ucode.com>
2019-02-03 16:42:02 -0800

    Added Algolia integration

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const ALGOLIA_ID = functions.config().algolia.app_id
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
+const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key
+
+const ALGOLIA_INDEX_NAME = 'decks'
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
+
+exports.deckCreated = functions.firestore.document('decks/{deckId}').onCreate((snapshot, context) => {
+    const deck = snapshot.data()
+    deck.objectID = context.params.deckId
+    const index = client.initIndex(ALGOLIA_INDEX_NAME)
+    return index.saveObject(deck)
+})
