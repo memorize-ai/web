d11ba0cb7e8658f026785b16431a5972911a799e
Ken Mueller <kenmueller0@gmail.com>
2019-02-26 18:58:49 -0800

    rules

--- functions/index.js
+++ functions/index.js
@@ -12,38 +12,46 @@ const ALGOLIA_ADMIN_KEY = env.algolia.api_key
 const ALGOLIA_INDEX_NAME = 'decks'
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
 const index = client.initIndex(ALGOLIA_INDEX_NAME)
+let db = firestore.firestore()
 
-const addKeyVal = obj => key => val => {
+function addKeyVal(obj, key, val) {
 	obj[key] = val
 	return obj
 }
 
-const updateDeckInAngolia = (snapshot, context) =>
-	index.saveObject(addKeyVal(snapshot.data())('objectID')(context.params.deckId))
+function updateDeckInAngolia(snapshot, context) {
+	index.saveObject(addKeyVal(snapshot.data(), 'objectID', context.params.deckId))
+}
 
-const deleteDeckInAngolia = snapshot =>
+function deleteDeckInAngolia(snapshot) {
 	index.deleteObject(snapshot.id)
+}
 
-const addToDate = date => elapsed =>
-	new Date(date.getTime() + elapsed)
+function addToDate(date, elapsed) {
+	return new Date(date.getTime() + elapsed)
+}
 
 exports.deckCreated = functions.firestore.document('decks/{deckId}').onCreate(updateDeckInAngolia)
 exports.deckUpdated = functions.firestore.document('decks/{deckId}').onUpdate(updateDeckInAngolia)
 exports.deckDeleted = functions.firestore.document('decks/{deckId}').onDelete(deleteDeckInAngolia)
 
 exports.cardCreated = functions.firestore.document('decks/{deckId}/cards/{cardId}').onCreate((_, context) => {
-	return admin.firestore().collection('decks').doc(context.params.deckId).get().then(deck => {
-		return admin.firestore().collection('decks').doc(context.params.deckId).update({ count: deck.data().count + 1 })
+	return db.collection('decks').doc(context.params.deckId).get().then(deck => {
+		return db.collection('decks').doc(context.params.deckId).update({ count: deck.data().count + 1 })
 	})
 })
 
+exports.permissionsCreated = functions.firestore.document('decks/{deckId}/permissions/{permissionId}').onCreate((_, context) => {
+	return db.collection('users').doc(context.params.permissionId).collection('decks').doc(context.params.deckId).set({ mastered: 0 })
+})
+
 exports.historyCreated = functions.firestore.document('users/{uid}/decks/{deckId}/cards/{cardId}/history/{historyId}').onCreate((snapshot, context) => {
-	return admin.firestore().collection('users').doc(context.params.uid).collection('decks').doc(context.params.deckId).collection('cards').doc(context.params.cardId).get().then(card => {
+	return db.collection('users').doc(context.params.uid).collection('decks').doc(context.params.deckId).collection('cards').doc(context.params.cardId).get().then(card => {
 		const elapsed = snapshot.date - card.data().last.getTime()
 		const next = addToDate(snapshot.date, snapshot.correct ? elapsed * 2 : 14400000)
 		return Promise.all([
-			admin.firestore().collection('users').doc(context.params.uid).collection('decks').doc(context.params.deckId).collection('cards').doc(context.params.cardId).collection('history').doc(context.params.historyId).update({ elapsed: elapsed, next: next }),
-			admin.firestore().collection('users').doc(context.params.uid).collection('decks').doc(context.params.deckId).collection('cards').doc(context.params.cardId).update({ last: snapshot.date, next: next })
+			db.collection('users').doc(context.params.uid).collection('decks').doc(context.params.deckId).collection('cards').doc(context.params.cardId).collection('history').doc(context.params.historyId).update({ elapsed: elapsed, next: next }),
+			db.collection('users').doc(context.params.uid).collection('decks').doc(context.params.deckId).collection('cards').doc(context.params.cardId).update({ last: snapshot.date, next: next })
 		])
 	})
 })
@@ -66,7 +74,7 @@ exports.historyCreated = functions.firestore.document('users/{uid}/decks/{deckId
 
 exports.userCreated = functions.firestore.document('users/{uid}').onCreate((snapshot, context) => {
 	return Promise.all([
-		admin.firestore().collection('emails').doc(snapshot.email.replace('@', '%2e')).setData({ id: context.params.uid }),
-		admin.firestore().collection('links').doc(snapshot.link).setData({ id: context.params.uid })
+		db.collection('emails').doc(snapshot.email.replace('@', '%2e')).setData({ id: context.params.uid }),
+		db.collection('links').doc(snapshot.link).setData({ id: context.params.uid })
 	])
 })
