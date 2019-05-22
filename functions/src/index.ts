7d4c878729a88be541e8b1f0b16fdd80ab7224d1
Ken Mueller <kenmueller0@gmail.com>
2019-05-22 00:33:02 -0700

    updated exports

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as admin from 'firebase-admin'
 admin.initializeApp()
 
-export { userCreated, userUpdated, userDeleted } from './User'
-export { deckCreated, deckUpdated, deckDeleted } from './Deck'
+export { userCreated, userUpdated, userDeleted, deckAdded, deckRemoved } from './User'
+export { deckCreated, deckUpdated, deckDeleted, viewDeck, rateDeck } from './Deck'
 export { cardCreated, cardDeleted } from './Card'
 export { permissionCreated, permissionDeleted } from './Permission'
 export { historyCreated } from './History'
