7ad1c2c7b8464f7c30ac0f4c09c5e629a050e3b1
Ken Mueller <kenmueller0@gmail.com>
2019-06-02 16:30:23 -0700

    added uploads to indices

--- functions/src/Algolia.ts
+++ functions/src/Algolia.ts
@@ -7,7 +7,8 @@ const client = algoliasearch(config.app_id, config.api_key)
 export default class Algolia {
 	static indices = {
 		decks: client.initIndex('decks'),
-		users: client.initIndex('users')
+		users: client.initIndex('users'),
+		uploads: client.initIndex('uploads')
 	}
 
 	private static save({ index, data, id }: { index: algoliasearch.Index, data: FirebaseFirestore.DocumentData, id: string }): Promise<algoliasearch.Task> {
