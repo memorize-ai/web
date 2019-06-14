57b7676bf07270c836ed1da85406f038e5345c23
Ken Mueller <kenmueller0@gmail.com>
2019-06-13 20:16:28 -0700

    fixed rating draft updated; all done with ratings

--- functions/src/RatingDraft.ts
+++ functions/src/RatingDraft.ts
@@ -10,25 +10,23 @@ export const ratingDraftCreated = functions.firestore.document('users/{uid}/rati
 
 export const ratingDraftUpdated = functions.firestore.document('users/{uid}/ratingDrafts/{deckId}').onUpdate((change, context) => {
 	const snapshot = change.after
-	const deleteSnapshot = snapshot.ref.delete
+	const deleteSnapshot = () => snapshot.ref.delete()
 	const handleField = (data_: FirebaseFirestore.DocumentData, field: string) =>
 		data_[field] ? Promise.resolve() as Promise<any> : snapshot.ref.update({ [field]: admin.firestore.FieldValue.delete() })
 	const data = snapshot.data()
 	return Promise.all([
 		User.updateLastActivity(context.params.uid),
-		data
-			? !(data.rating || data.review)
-				? deleteSnapshot()
-				: Promise.all([
-					handleField(data, 'rating'),
-					handleField(data, 'title'),
-					handleField(data, 'review'),
-					Deck.doc(context.params.deckId, `users/${context.params.uid}`).get().then(user =>
-						snapshot.get('rating') === user.get('rating') && snapshot.get('title') === user.get('title') && snapshot.get('review') === user.get('review')
-							? deleteSnapshot()
-							: Promise.resolve() as Promise<any>
-					)
-				])
+		data && (data.rating || data.review)
+			? Promise.all([
+				handleField(data, 'rating'),
+				handleField(data, 'title'),
+				handleField(data, 'review'),
+				Deck.doc(context.params.deckId, `users/${context.params.uid}`).get().then(user =>
+					snapshot.get('rating') === user.get('rating') && snapshot.get('title') === user.get('title') && snapshot.get('review') === user.get('review')
+						? deleteSnapshot()
+						: Promise.resolve() as Promise<any>
+				)
+			])
 			: deleteSnapshot() as Promise<any>
 	])
 })
