d8c4afca5648301d896bb4e45da15b56ddf458f3
Ken Mueller <kenmueller0@gmail.com>
2019-06-14 21:50:00 -0700

    cleanData

--- functions/src/Admin.ts
+++ functions/src/Admin.ts
@@ -19,15 +19,86 @@ export default class Admin {
 		const value = secure.newId(100)
 		return Admin.doc('key').set({ value }).then(() => value)
 	}
+
+	static httpsCheckKey(req: functions.https.Request, res: functions.Response, success: () => void) {
+		const key = req.query.key
+		return key
+			? Admin.checkKey(key).then(isValid =>
+				isValid
+					? success()
+					: res.status(401).send('Invalid admin key')
+			)
+			: res.status(400).send('You must specify the current admin key')
+	}
 }
 
-export const resetAdminKey = functions.https.onRequest((req, res) => {
-	const key = req.query.key
-	return key
-		? Admin.checkKey(key).then(isValid =>
-			isValid
-				? Admin.resetKey().then(newKey => res.status(200).send(newKey))
-				: res.status(401).send('Invalid admin key')
+export const resetAdminKey = functions.https.onRequest((req, res) =>
+	Admin.httpsCheckKey(req, res, () =>
+		Admin.resetKey().then(newKey =>
+			res.status(200).send(newKey)
 		)
-		: res.status(400).send('You must specify the current admin key')
-})
+	)
+)
+
+export const cleanData = functions.https.onRequest((req, res) =>
+	Admin.httpsCheckKey(req, res, () => {
+		const now = new Date
+		return Promise.all([
+			firestore.collection('decks').listDocuments().then(decks =>
+				Promise.all(decks.map(deck =>
+					Promise.all([
+						deck.update({
+							subtitle: '',
+							tags: [],
+							views: {
+								total: 0,
+								unique: 0
+							},
+							downloads: {
+								total: 0,
+								current: 0
+							},
+							ratings: {
+								average: 0,
+								1: 0,
+								2: 0,
+								3: 0,
+								4: 0,
+								5: 0
+							},
+							created: now,
+							updated: now
+						}),
+						deck.collection('cards').listDocuments().then(cards =>
+							Promise.all(cards.map(card =>
+								card.update({
+									created: now,
+									updated: now,
+									likes: 0,
+									dislikes: 0
+								})
+							))
+						)
+					])
+				))
+			),
+			firestore.collection('users').listDocuments().then(users =>
+				Promise.all(users.map(user =>
+					Promise.all([
+						user.update({
+							lastNotification: 0,
+							joined: now,
+							lastOnline: now,
+							lastActivity: now
+						}),
+						user.collection('decks').listDocuments().then(decks =>
+							Promise.all(decks.map(deck =>
+								deck.delete()
+							))
+						)
+					])
+				))
+			)
+		])
+	})
+)
