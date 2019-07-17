0017ee19ffc7cab27f28dadb1cd55dafcdd1bc1f
Ken Mueller <kenmueller0@gmail.com>
2019-07-16 19:28:34 -0700

    decksDue

--- functions/src/CardNotification.ts
+++ functions/src/CardNotification.ts
@@ -25,7 +25,8 @@ export const checkDueCards = functions.pubsub.schedule('every 15 minutes').onRun
 	).then(sendNotifications)
 })
 
-function getUser(user: FirebaseFirestore.DocumentSnapshot, date: number = Date.now()): Promise<{ uid: string, tokens: string[], cardsDue: number }> {
+function getUser(user: FirebaseFirestore.DocumentSnapshot, date: number = Date.now()): Promise<{ uid: string, tokens: string[], decksDue: number, cardsDue: number }> {
+	const decksDue = new Set<string>()
 	return User.getTokens(user.id).then(tokens => (
 		tokens.length
 			? User.getLastNotificationDifference(user, date) < LAST_NOTIFICATION_DIFFERENCE
@@ -34,9 +35,11 @@ function getUser(user: FirebaseFirestore.DocumentSnapshot, date: number = Date.n
 					Promise.all(decks.docs.filter(deck => !deck.get('hidden')).map(deck =>
 						Deck.collection(deck.id, 'cards').listDocuments().then(cards =>
 							Promise.all(cards.map(emptyCard =>
-								firestore.doc(`users/${user.id}/decks/${deck.id}/cards/${emptyCard.id}`).get().then(card =>
-									(Card.isDue(card, date) ? 1 : 0) as number
-								)
+								firestore.doc(`users/${user.id}/decks/${deck.id}/cards/${emptyCard.id}`).get().then(card => {
+									const isDue = Card.isDue(card, date)
+									if (isDue) decksDue.add(deck.id)
+									return (isDue ? 1 : 0) as number
+								})
 							))
 						)
 					))
@@ -47,10 +50,15 @@ function getUser(user: FirebaseFirestore.DocumentSnapshot, date: number = Date.n
 	).then(cardsDue => ({
 		uid: user.id,
 		tokens,
+		decksDue: decksDue.size,
 		cardsDue
 	})))
 }
 
-function sendNotifications(users: { uid: string, tokens: string[], cardsDue: number }[]): Promise<admin.messaging.BatchResponse> {
-	return Notification.sendAll(users.map())
+function sendNotifications(users: { uid: string, tokens: string[], decksDue: number, cardsDue: number }[]): Promise<admin.messaging.BatchResponse> {
+	return Notification.sendAll(users.map(user => 
+		user.tokens.map(token =>
+			new Notification(token, `You have ${user.cardsDue} card${user.cardsDue === 1 ? '' : 's'} to review`, `You have ${user.cardsDue} card${user.cardsDue === 1 ? '' : 's'} to review in ${user.decksDue} deck${user.decksDue === 1 ? '' : 's'}`)
+		)
+	).flat())
 }
