35dcbb12415f915d5e26d184e6debe884bbd5894
Ken Mueller <kenmueller0@gmail.com>
2019-07-19 20:45:32 -0700

    added reputation enum values

--- functions/src/Reputation.ts
+++ functions/src/Reputation.ts
@@ -15,6 +15,18 @@ export enum ReputationAction {
 	join = 'join',
 	rateDeck = 'rate-deck',
 	rateDeckWithReview = 'rate-deck-with-review',
+	everyFollower = 'every-follower',
+	every10Followers = 'every-10-followers',
+	every50Followers = 'every-50-followers',
+	every100Followers = 'every-100-followers',
+	everyCardReviewed = 'every-card-reviewed',
+	every10CardsReviewed = 'every-10-cards-reviewed',
+	every50CardsReviewed = 'every-50-cards-reviewed',
+	every100CardsReviewed = 'every-100-cards-reviewed',
 	didGetCardLike = 'did-get-card-like',
-	didGetCardDislike = 'did-get-card-dislike'
+	didGetCardDislike = 'did-get-card-dislike',
+	didGet1StarDeckRating = 'did-get-1-star-deck-rating',
+	didGet2StarDeckRating = 'did-get-2-star-deck-rating',
+	didGet4StarDeckRating = 'did-get-4-star-deck-rating',
+	didGet5StarDeckRating = 'did-get-5-star-deck-rating'
 }
