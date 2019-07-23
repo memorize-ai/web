c1fae9b7e939e6c71968fd6d4a62471ac336cbde
Ken Mueller <kenmueller0@gmail.com>
2019-07-22 19:55:28 -0700

    getUserDecks

--- functions/src/SignInToken.ts
+++ functions/src/SignInToken.ts
@@ -2,6 +2,8 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as secure from 'securejs'
 
+import Deck from './Deck'
+
 const firestore = admin.firestore()
 const auth = admin.auth()
 
@@ -23,18 +25,46 @@ export const getUserSignInToken = functions.https.onRequest((req, res) => {
 export const checkUserSignInToken = functions.https.onRequest((req, res) => {
 	const uid = req.query.uid
 	const token = req.query.token
-	if (!(uid && token)) return res.status(400).send('Specify a uid and token')
-	const doc = firestore.doc(`users/${uid}/signInTokens/${token}`)
-	return doc.get().then(snapshot =>
-		snapshot.exists
-			? doc.update({ date: new Date }).then(_writeResult =>
-				firestore.doc(`users/${uid}`).get().then(user =>
+	return uid && token
+		? checkSignInToken(uid, token).then(valid =>
+			valid
+				? firestore.doc(`users/${uid}`).get().then(user =>
 					res.status(200).send(user.data())
+				)
+				: res.status(404).send('Invalid token')
+		)
+		: res.status(400).send('Specify a uid and token')
+})
+
+export const getUserDecks = functions.https.onRequest((req, res) => {
+	const uid = req.query.uid
+	const token = req.query.token
+	if (!(uid && token)) return res.status(400).send('Specify a uid and token')
+	return checkSignInToken(uid, token).then(valid =>
+		valid
+			? firestore.collection(`users/${uid}/decks`).where('hidden', '==', false).get().then(userDecks =>
+				Promise.all(userDecks.docs.map(userDeck =>
+					firestore.doc(`decks/${userDeck.id}`).get().then(deck =>
+						Deck.image(deck).then(image =>
+							Object.assign(deck.data() || {}, { image, userData: userDeck.data() })
+						)
+					)
+				)).then(decks =>
+					res.status(200).send(decks)
 				)
 			)
 			: res.status(404).send('Invalid token')
 	)
 })
+
+function checkSignInToken(uid: string, token: string): Promise<boolean> {
+	const doc = firestore.doc(`users/${uid}/signInTokens/${token}`)
+	return doc.get().then(snapshot =>
+		snapshot.exists
+			? doc.update({ date: new Date }).then(_writeResult => true)
+			: false
+	)
+}
 
 function checkPasswordForUser(user: admin.auth.UserRecord, password: string): boolean {
 	return user.passwordHash === password
