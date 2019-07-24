72a8606f90724efe1ba55804b6e383cee1d0cc48
Ken Mueller <kenmueller0@gmail.com>
2019-07-23 18:19:09 -0700

    flatten array in Helpers.ts

--- functions/src/Helpers.ts
+++ functions/src/Helpers.ts
@@ -2,3 +2,9 @@ export function getDate(snapshot: FirebaseFirestore.DocumentSnapshot, field: str
 	const timestamp: FirebaseFirestore.Timestamp | undefined = snapshot.get(field)
 	return timestamp ? timestamp.toDate() : undefined
 }
+
+export function flatten<T>(array: any[], depth: number): T[] {
+	return array.reduce((acc, element) =>
+		acc.concat(Array.isArray(element) && depth > 1 ? flatten(element, depth - 1) : element)
+	, [])
+}
