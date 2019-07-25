f7eaecb4fc00fe02ffb42f58440bddd88aa3fb1e
Ken Mueller <kenmueller0@gmail.com>
2019-07-25 16:30:35 -0700

    addAnalytics no decode

--- functions/src/Analytics.ts
+++ functions/src/Analytics.ts
@@ -1,18 +1,18 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
+import { getQueryParameter } from './Helpers'
+
 const firestore = admin.firestore()
 
 export const addAnalytics = functions.https.onCall((data, _context) => {
-	const category: string | undefined = data.category
-	const id: string | undefined = data.id
+	const category = getQueryParameter(data, 'category')
+	const id = getQueryParameter(data, 'id', false)
 	return category && category.length && id && id.length
-		? firestore.doc(`analytics/${category}`).get().then(snapshot => {
-			const encodedId = id.replace(/\//g, '%2F').replace(/\./g, '%2E')
-			const ref = snapshot.ref
-			return snapshot.exists
-				? ref.update({ [encodedId]: snapshot.get(encodedId) ? admin.firestore.FieldValue.increment(1) : 1 })
-				: ref.set({ [encodedId]: 1 })
-		})
+		? firestore.doc(`analytics/${category}`).get().then(snapshot =>
+			snapshot.exists
+				? snapshot.ref.update({ [id]: snapshot.get(id) ? admin.firestore.FieldValue.increment(1) : 1 })
+				: snapshot.ref.set({ [id]: 1 })
+		)
 		: new functions.https.HttpsError('invalid-argument', 'You must specify an action and url')
 })
