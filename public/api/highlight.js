b78282c5bb909429c5b4f46554bf91f6a91c2741
Ken Mueller <kenmueller0@gmail.com>
2019-07-24 18:07:35 -0700

    highlight.js mouseUp and mouseDown functions are called

--- public/api/highlight.js
+++ public/api/highlight.js
@@ -1,4 +1,4 @@
-(() => {
+document.addEventListener('DOMContentLoaded', () => {
 	const UID_COOKIE = '__memorize-ai-uid'
 	const NAME_COOKIE = '__memorize-ai-name'
 	const TOKEN_COOKIE = '__memorize-ai-token'
@@ -7,9 +7,14 @@
 		
 	}
 
+	function mouseDown() {
+		
+	}
+
 	function getSelectedText() {
 		return document.all ? document.selection.createRange().text : document.getSelection()
 	}
 
 	document.querySelectorAll('.memorize-ai-highlightable').forEach(element => element.onmouseup = mouseUp)
-})()
+	document.onmousedown = mouseDown
+})
