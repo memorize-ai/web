67f081163c05a632b77ead6db10cd53827cfcbab
Ken Mueller <kenmueller0@gmail.com>
2019-07-24 23:54:15 -0700

    fixed addAnalytics

--- functions/src/Analytics.ts
+++ functions/src/Analytics.ts
@@ -8,8 +8,11 @@ export const addAnalytics = functions.https.onCall((data, _context) => {
 	const id: string | undefined = data.id
 	return category && category.length && id && id.length
 		? firestore.doc(`analytics/${category}`).get().then(snapshot => {
-			const encodedId = id.replace('/', '%2F')
-			return snapshot.ref.update({ [encodedId]: snapshot.get(encodedId) === undefined ? 1 : admin.firestore.FieldValue.increment(1) })
+			const encodedId = id.replace(/\//g, '%2F').replace(/\./g, '%2E')
+			const ref = snapshot.ref
+			return snapshot.exists
+				? ref.update({ [encodedId]: snapshot.get(encodedId) ? admin.firestore.FieldValue.increment(1) : 1 })
+				: ref.set({ [encodedId]: 1 })
 		})
 		: new functions.https.HttpsError('invalid-argument', 'You must specify an action and url')
 })
