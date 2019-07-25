d357b0f75ea58159d709b1512aa7043c07ca706f
Ken Mueller <kenmueller0@gmail.com>
2019-07-24 20:34:26 -0700

    working on highlight.js

--- public/api/highlight.js
+++ public/api/highlight.js
@@ -3,18 +3,53 @@ document.addEventListener('DOMContentLoaded', () => {
 	const NAME_COOKIE = '__memorize-ai-name'
 	const TOKEN_COOKIE = '__memorize-ai-token'
 
-	function mouseUp() {
-		
+	function getCookie(name) {
+		const cookies = document.cookie.match(`(^|[^;]+)\\s*${name}\\s*=\\s*([^;]+)`)
+		return cookies ? cookies.pop() : undefined
+	}
+	
+	function setCookie(name, value) {
+		document.cookie = `${name}=${value}; expires=Thu, 01 Jan 3000 00:00:00 GMT`
+	}
+	
+	function removeCookie(name) {
+		document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`
+	}
+	
+	function mouseUp(event) {
+		const selectedText = getSelectedText()
+		if (!selectedText.length) return
+		showModal(selectedText, event)
 	}
 
-	function mouseDown() {
-		
+	function callOnModals(fn) {
+		document.querySelectorAll('div.__memorize-ai-highlight-modal.__memorize-ai-protected').forEach(fn)
+	}
+
+	function showModal(text, event) {
+		hideModal()
+		const div = document.createElement('div')
+		div.classList.add('__memorize-ai-highlight-modal')
+		div.classList.add('__memorize-ai-protected')
+		if (getCookie(UID_COOKIE)) {
+
+		} else {
+			div.innerHTML = '<a href="https://memorize.ai/sign-up?from=create-card" target="_blank">Sign up</a><br><br><a class="__memorize-ai-create-card-sign-in __memorize-ai-protected">Sign in</a>'
+		}
+		div.style.left = `${event.clientX}px`
+		div.style.top = `${event.clientY}px`
+		div.style.position = 'absolute'
+		document.querySelector('*').append(div)
+	}
+
+	function hideModal() {
+		callOnModals(element => document.querySelector('*').removeChild(element))
 	}
 
 	function getSelectedText() {
-		return document.all ? document.selection.createRange().text : document.getSelection()
+		return (document.all ? document.selection.createRange().text : document.getSelection()).toString()
 	}
 
 	document.querySelectorAll('.memorize-ai-highlightable').forEach(element => element.onmouseup = mouseUp)
-	document.onmousedown = mouseDown
+	document.querySelectorAll('.memorize-ai-show-create-card-modal').forEach(element => element.addEventListener('click', event => showModal(null, event)))
 })
