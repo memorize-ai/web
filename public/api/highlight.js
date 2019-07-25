758972fc9fe7be664a6a0adabf3b6e9d3e05e3af
Ken Mueller <kenmueller0@gmail.com>
2019-07-25 16:59:27 -0700

    working on highlight.js fetch

--- public/api/highlight.js
+++ public/api/highlight.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		? { width: MEMORIZE_AI_CREATE_CARD_MODAL_DIMENSIONS.width || 100, height: MEMORIZE_AI_CREATE_CARD_MODAL_DIMENSIONS.height || 100 }
 		: { width: 100, height: 100 }
 
+	let token
+
 	function getCookie(name) {
 		const cookies = document.cookie.match(`(^|[^;]+)\\s*${name}\\s*=\\s*([^;]+)`)
 		return cookies ? cookies.pop() : undefined
@@ -40,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			div.innerHTML = `
 			<button class="__memorize-ai-create-card-close-modal __memorize-ai-protected">Close</button>
 			<button class="__memorize-ai-create-card-disable-modal __memorize-ai-protected">Disable</button>
-			<a href="https://memorize.ai/sign-up?from-action=create-card&from-url=${location.href}" target="_blank">Sign up</a
+			<a href="https://memorize.ai/sign-up?from-action=create-card&from-url=${encodeURIComponent(location.href)}" target="_blank">Sign up</a
 			<br><br>
 			<a class="__memorize-ai-create-card-sign-in __memorize-ai-protected" href="#">Sign in</a>`
 		}
@@ -71,6 +73,52 @@ document.addEventListener('DOMContentLoaded', () => {
 	function getSelectedText() {
 		return (document.all ? document.selection.createRange().text : document.getSelection()).toString()
 	}
+
+	function signIn(email, password) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-memorize-ai.cloudfunctions.net/getUserSignInToken?password=${encodeURIComponent(btoa(password))}&email=${encodeURIComponent(email)}`).then(response => {
+			switch (response.status) {
+			case 200:
+				return response.json()
+			case 401:
+				return Promise.reject('Invalid password')
+			case 403:
+				return Promise.reject(`No user with email ${email}. Would you like to <a href="https://memorize.ai/sign-up?from-action=create-card&from-url=${encodeURIComponent(location.href)}" target="_blank">sign up</a>?`)
+			default:
+				return Promise.reject('An unknown error occurred. Please try again')
+			}
+		})
+	}
+
+	function getUserFromToken(uid, token) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-memorize-ai.cloudfunctions.net/checkUserSignInToken?uid=${encodeURIComponent(uid)}&token=${encodeURIComponent(token)}`).then(response => {
+			switch (response.status) {
+			case 200:
+				return response.json()
+			case 404:
+				return Promise.reject('invalid-token')
+			default:
+				return Promise.reject('unknown-error')
+			}
+		})
+	}
+
+	function getDecks(uid, token) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-memorize-ai.cloudfunctions.net/getUserDecks?uid=${encodeURIComponent(uid)}&token=${encodeURIComponent(token)}`).then(response => {
+			switch (response.status) {
+			case 200:
+				return response.json()
+			case 404:
+				return Promise.reject('invalid-token')
+			default:
+				return Promise.reject('unknown-error')
+			}
+		})
+	}
+
+	function createCard(uid, token, deckId, front, back) {
+		return fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-memorize-ai.cloudfunctions.net/createCardWithSignInToken?uid=${encodeURIComponent(uid)}&token=${encodeURIComponent(token)}`)
+	}
+
 	if (MEMORIZE_AI_HIGHLIGHT_ENABLED === undefined ? true : MEMORIZE_AI_HIGHLIGHT_ENABLED)
 		document.querySelectorAll('.memorize-ai-highlightable').forEach(element => element.onmouseup = mouseUp)
 	document.querySelectorAll('.memorize-ai-show-create-card-modal').forEach(element => element.addEventListener('click', event => showModal(null, event)))
