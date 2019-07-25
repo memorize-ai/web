019c44b1164ff3eafff3e80af05cde5a5d09dfd9
Ken Mueller <kenmueller0@gmail.com>
2019-07-24 22:52:37 -0700

    encodedId

--- functions/src/Analytics.ts
+++ functions/src/Analytics.ts
@@ -7,8 +7,9 @@ export const addAnalytics = functions.https.onCall((data, _context) => {
 	const category: string | undefined = data.category
 	const id: string | undefined = data.id
 	return category && category.length && id && id.length
-		? firestore.doc(`analytics/${category}`).get().then(snapshot =>
-			snapshot.ref.update({ [id]: snapshot.get(id) === undefined ? 1 : admin.firestore.FieldValue.increment(1) })
-		)
+		? firestore.doc(`analytics/${category}`).get().then(snapshot => {
+			const encodedId = id.replace('/', '%2F')
+			return snapshot.ref.update({ [encodedId]: snapshot.get(encodedId) === undefined ? 1 : admin.firestore.FieldValue.increment(1) })
+		})
 		: new functions.https.HttpsError('invalid-argument', 'You must specify an action and url')
 })
