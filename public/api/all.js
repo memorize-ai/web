9520590305a4cefb6f7573e4870cd2d7200f643e
Ken Mueller <kenmueller0@gmail.com>
2019-08-09 22:27:47 -0700

    var to const

--- public/api/all.js
+++ public/api/all.js
@@ -2,7 +2,7 @@
 	addScriptTagToHead('highlight')
 
 	function addScriptTagToHead(src) {
-		var script = document.createElement('script')
+		const script = document.createElement('script')
 		script.src = `https://memorize.ai/api/${src}`
 		document.head.appendChild()
 	}
