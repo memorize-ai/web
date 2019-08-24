6386caadd35cb97caf2bb6d804c7e1c3c41a2846
Ken Mueller <kenmueller0@gmail.com>
2019-08-24 14:12:04 -0700

    removed redundant ternary

--- functions/src/Analytics.ts
+++ functions/src/Analytics.ts
@@ -11,7 +11,7 @@ export const addAnalytics = functions.https.onCall((data, _context) => {
 	return category && category.length && id && id.length
 		? firestore.doc(`analytics/${category}`).get().then(snapshot =>
 			snapshot.exists
-				? snapshot.ref.update({ [id]: snapshot.get(id) ? admin.firestore.FieldValue.increment(1) : 1 })
+				? snapshot.ref.update({ [id]: admin.firestore.FieldValue.increment(1) })
 				: snapshot.ref.set({ [id]: 1 })
 		)
 		: new functions.https.HttpsError('invalid-argument', 'You must specify an action and url')
