34d221472076c7b1340781b0c9748611d97780fa
Ken Mueller <kenmueller0@gmail.com>
2019-10-25 11:33:44 -0700

    returns null instead of undefined

--- functions/src/User/userUpdated.ts
+++ functions/src/User/userUpdated.ts
@@ -5,9 +5,9 @@ const auth = admin.auth()
 
 export default functions.firestore.document('users/{uid}').onUpdate((change, context) => {
 	const newName = change.after.get('name')
-	return change.before.get('name') === newName || (
-		context.auth && updateDisplayName(context.auth.uid, newName)
-	)
+	return change.before.get('name') !== newName && context.auth
+		? updateDisplayName(context.auth.uid, newName)
+		: null
 })
 
 const updateDisplayName = (uid: string, displayName: string): Promise<admin.auth.UserRecord> =>
