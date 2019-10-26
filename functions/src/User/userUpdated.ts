6d5ac87d5174352a6ad3432f4aa5c0ccfb8a5d67
Ken Mueller <kenmueller0@gmail.com>
2019-10-25 17:28:13 -0700

    fixed userUpdated

--- functions/src/User/userUpdated.ts
+++ functions/src/User/userUpdated.ts
@@ -3,12 +3,9 @@ import * as admin from 'firebase-admin'
 
 const auth = admin.auth()
 
-export default functions.firestore.document('users/{uid}').onUpdate((change, context) => {
+export default functions.firestore.document('users/{uid}').onUpdate((change, { params: { uid } }) => {
 	const newName = change.after.get('name')
-	return change.before.get('name') !== newName && context.auth
-		? updateDisplayName(context.auth.uid, newName)
-		: null
+	return change.before.get('name') === newName
+		? null
+		: auth.updateUser(uid, { displayName: newName })
 })
-
-const updateDisplayName = (uid: string, displayName: string): Promise<admin.auth.UserRecord> =>
-	auth.updateUser(uid, { displayName })
