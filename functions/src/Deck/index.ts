2e53b7a4ef687f368e245e4352e6ec7395030def
Ken Mueller <kenmueller0@gmail.com>
2019-11-01 22:28:18 -0700

    applies slicing

--- functions/src/Deck/index.ts
+++ functions/src/Deck/index.ts
@@ -45,7 +45,8 @@ export default class Deck {
 				topic.getTopDecks().then(topDecks => {
 					for (const i of [...topDecks.keys()])
 						if (this.compareTo(topDecks[i])) {
-							topDecks = topDecks.splice(i, 0, this).slice(0, Topic.MAX_TOP_DECKS_LENGTH)
+							topDecks.splice(i, 0, this)
+							topDecks = topDecks.slice(0, Topic.MAX_TOP_DECKS_LENGTH)
 							return topic.documentReference.update({
 								topDecks: topDecks.map(({ id }) => id)
 							})
