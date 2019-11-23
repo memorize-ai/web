d5a2f6f5076a5bd30fe7eac6d9d6fdc1c924465c
Ken Mueller <kenmueller0@gmail.com>
2019-11-23 13:02:03 -0500

    deck created index

--- functions/src/Deck/functions/deckCreated.ts
+++ functions/src/Deck/functions/deckCreated.ts
@@ -2,6 +2,10 @@ import * as functions from 'firebase-functions'
 
 import Deck from '..'
 
-export default functions.firestore.document('decks/{deckId}').onCreate(snapshot =>
-	new Deck(snapshot).insertIntoTopDecks()
-)
+export default functions.firestore.document('decks/{deckId}').onCreate(snapshot => {
+	const deck = new Deck(snapshot)
+	return Promise.all([
+		deck.index(),
+		deck.insertIntoTopDecks()
+	])
+})
