489ad3982804c05b24ce3eae380b349ca9e57e7f
Ken Mueller <kenmueller0@gmail.com>
2019-11-23 13:03:18 -0500

    deck updated index

--- functions/src/Deck/functions/deckUpdated.ts
+++ functions/src/Deck/functions/deckUpdated.ts
@@ -2,6 +2,10 @@ import * as functions from 'firebase-functions'
 
 import Deck from '..'
 
-export default functions.firestore.document('decks/{deckId}').onUpdate(({ after: snapshot }) =>
-	new Deck(snapshot).insertIntoTopDecks()
-)
+export default functions.firestore.document('decks/{deckId}').onUpdate(({ after: snapshot }) => {
+	const deck = new Deck(snapshot)
+	return Promise.all([
+		deck.index(),
+		deck.insertIntoTopDecks()
+	])
+})
