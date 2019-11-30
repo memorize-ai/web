d7ed7cd36f1a5cc547189700cfc9a914828a30da
Ken Mueller <kenmueller0@gmail.com>
2019-11-30 12:49:25 -0800

    section card count

--- functions/src/Card/index.ts
+++ functions/src/Card/index.ts
@@ -23,4 +23,14 @@ export default class Card {
 		firestore.doc(`decks/${deckId}/cards/${cardId}`).get().then(snapshot =>
 			new Card(snapshot)
 		)
+	
+	incrementSectionCardCount = (deckId: string): Promise<FirebaseFirestore.WriteResult> =>
+		firestore.doc(`decks/${deckId}/sections/${this.sectionId}`).update({
+			cardCount: admin.firestore.FieldValue.increment(1)
+		})
+	
+	decrementSectionCardCount = (deckId: string): Promise<FirebaseFirestore.WriteResult> =>
+		firestore.doc(`decks/${deckId}/sections/${this.sectionId}`).update({
+			cardCount: admin.firestore.FieldValue.increment(-1)
+		})
 }
