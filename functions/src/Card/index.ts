bc55f2a02eb03a29623ed5314d1af164c72af630
Ken Mueller <kenmueller0@gmail.com>
2019-11-30 12:45:16 -0800

    created Card

---
+++ functions/src/Card/index.ts
@@ -0,0 +1,26 @@
+import * as admin from 'firebase-admin'
+
+const firestore = admin.firestore()
+
+export default class Card {
+	id: string
+	sectionId: string
+	front: string
+	back: string
+	numberOfViews: number
+	numberOfSkips: number
+	
+	constructor(snapshot: FirebaseFirestore.DocumentSnapshot) {
+		this.id = snapshot.id
+		this.sectionId = snapshot.get('section')
+		this.front = snapshot.get('front')
+		this.back = snapshot.get('back')
+		this.numberOfViews = snapshot.get('viewCount') || 0
+		this.numberOfSkips = snapshot.get('skipCount') || 0
+	}
+	
+	static fromId = (cardId: string, deckId: string): Promise<Card> =>
+		firestore.doc(`decks/${deckId}/cards/${cardId}`).get().then(snapshot =>
+			new Card(snapshot)
+		)
+}
