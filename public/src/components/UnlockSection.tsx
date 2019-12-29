328f1e39eca865fcaf46121a0c15670630aa33c8
Ken Mueller <kenmueller0@gmail.com>
2019-12-28 23:42:54 -0700

    layout

--- public/src/components/UnlockSection.tsx
+++ public/src/components/UnlockSection.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Heading } from 'react-bulma-components'
+import { Heading, Box, Columns, Button } from 'react-bulma-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock, faUnlock } from '@fortawesome/free-solid-svg-icons'
 
 import useDeck from '../hooks/useDeck'
 import useSection from '../hooks/useSection'
@@ -16,6 +18,33 @@ export default () => {
 	return (
 		<div id="unlock-section">
 			<Heading textColor="white">{deck?.name}</Heading>
+			<Columns className="is-desktop is-vcentered">
+				<Columns.Column size="half" offset="one-quarter">
+					<Box id="content-box">
+						<Heading>Unlock {section?.name}</Heading>
+						<hr />
+						<div className="field">
+							<p className="control has-icons-left">
+								<input className="input" type="email" placeholder="Email" />
+								<span className="icon is-small is-left">
+									<FontAwesomeIcon icon={faEnvelope} />
+								</span>
+							</p>
+						</div>
+						<div className="field">
+							<p className="control has-icons-left">
+								<input className="input" type="password" placeholder="Password" />
+								<span className="icon is-small is-left">
+									<FontAwesomeIcon icon={faLock} />
+								</span>
+							</p>
+						</div>
+						<Button id="unlock-button" className="is-info" outlined>
+							<FontAwesomeIcon icon={faUnlock} />
+						</Button>
+					</Box>
+				</Columns.Column>
+			</Columns>
 		</div>
 	)
 }
