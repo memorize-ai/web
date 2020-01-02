4fe7caa37b4eae05657024141a88835422c0681d
Ken Mueller <kenmueller0@gmail.com>
2020-01-01 18:30:25 -0700

    updated exports

--- functions/src/Card/functions/index.ts
+++ functions/src/Card/functions/index.ts
@@ -1,4 +1,5 @@
 export { default as cardCreated } from './cardCreated'
 export { default as cardUpdated } from './cardUpdated'
 export { default as cardDeleted } from './cardDeleted'
+export { default as reviewCard } from './reviewCard'
 export { default as getCardPrediction } from './getCardPrediction'
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,6 +19,7 @@ export {
 	cardCreated,
 	cardUpdated,
 	cardDeleted,
+	reviewCard,
 	getCardPrediction
 } from './Card/functions'
 
