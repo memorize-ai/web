9137d7b7876346211973a11921aadc8b810f8919
Ken Mueller <kenmueller0@gmail.com>
2020-02-08 01:21:00 -0800

    build ios libraries

---
+++ ios-libraries/index.js
@@ -0,0 +1,50 @@
+const { join } = require('path')
+const {
+	mkdirSync: mkdir,
+	readFileSync: readFile,
+	writeFileSync: writeFile
+} = require('fs')
+
+const minifyCss = require('uglifycss').processString
+const minifyJs = require('babel-minify')
+
+mkdir(join(__dirname, 'lib'))
+
+const tree = {
+	display: {
+		css: [
+			readFile(join(__dirname, 'src/display/css/ckeditor-content-styles.css')).toString(),
+			readFile(join(__dirname, 'src/display/css/katex.css')).toString(),
+			readFile(join(__dirname, 'src/display/css/prism.css')).toString(),
+			readFile(join(__dirname, 'src/display/css/custom.css')).toString()
+		],
+		js: [
+			readFile(join(__dirname, 'src/display/js/katex.js')).toString(),
+			readFile(join(__dirname, 'src/display/js/auto-render.js')).toString(),
+			readFile(join(__dirname, 'src/display/js/prism.js')).toString()
+		]
+	},
+	editor: {
+		css: [
+			readFile(join(__dirname, 'src/editor/css/custom.css')).toString()
+		],
+		js: [
+			readFile(join(__dirname, 'src/editor/js/ckeditor.js')).toString()
+		]
+	}
+}
+
+for (const bundle of ['display', 'editor']) {
+	mkdir(join(__dirname, `lib/${bundle}`))
+	
+	const { css, js } = tree[bundle]
+	
+	writeFile(
+		join(__dirname, `lib/${bundle}/${bundle}.css`),
+		minifyCss(css.join('\n'))
+	)
+	writeFile(
+		join(__dirname, `lib/${bundle}/${bundle}.js`),
+		minifyJs(js.join('\n'), { mangle: false }).code || ''
+	)
+}
