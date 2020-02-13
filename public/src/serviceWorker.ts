4459bd16351028c46aad501216f3e067df22e35c
Ken Mueller <kenmueller0@gmail.com>
2020-02-13 10:52:23 -0800

    service worker

--- public/src/serviceWorker.ts
+++ public/src/serviceWorker.ts
@@ -10,21 +10,26 @@ interface Config {
 }
 
 export const register = (config?: Config) => {
-	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
-		if (publicUrl.origin !== window.location.origin)
-			return
-		window.addEventListener('load', () => {
-			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-			if (isLocalhost) {
-				checkValidServiceWorker(swUrl, config)
-				navigator.serviceWorker.ready.then(() =>
-					console.log('This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA')
-				)
-			} else
-				registerValidSW(swUrl, config)
-		})
-	}
+	if (!(process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator))
+		return
+	
+	const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
+	
+	if (publicUrl.origin !== window.location.origin)
+		return
+	
+	window.addEventListener('load', async () => {
+		const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+		
+		if (isLocalhost) {
+			checkValidServiceWorker(swUrl, config)
+			
+			await navigator.serviceWorker.ready
+			
+			console.log('This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA')
+		} else
+			registerValidSW(swUrl, config)
+	})
 }
 
 const registerValidSW = (swUrl: string, config?: Config) =>
