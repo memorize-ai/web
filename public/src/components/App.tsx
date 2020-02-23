9c2aa295acb5e08e7867c94996609e9681c7b999
Ken Mueller <kenmueller0@gmail.com>
2020-02-22 23:44:29 -1000

    privacy

--- public/src/components/App.tsx
+++ public/src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Privacy from './Privacy'
 import Support from './Support'
 import GetDeck from './GetDeck'
 import UnlockSection from './UnlockSection'
@@ -10,6 +11,7 @@ export default () => (
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={Home} />
+			<Route exact path="/privacy" component={Privacy} />
 			<Route exact path="/support" component={Support} />
 			<Route exact path="/d/:deckId/g" component={GetDeck} />
 			<Route exact path="/d/:deckId/s/:sectionId/u" component={UnlockSection} />
