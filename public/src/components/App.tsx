603cf3995ddf8cfb9489d6655ec06c49440c662b
Ken Mueller <kenmueller0@gmail.com>
2020-02-22 23:39:28 -1000

    support base

--- public/src/components/App.tsx
+++ public/src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Support from './Support'
 import GetDeck from './GetDeck'
 import UnlockSection from './UnlockSection'
 
@@ -9,6 +10,7 @@ export default () => (
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={Home} />
+			<Route exact path="/support" component={Support} />
 			<Route exact path="/d/:deckId/g" component={GetDeck} />
 			<Route exact path="/d/:deckId/s/:sectionId/u" component={UnlockSection} />
 		</Switch>
