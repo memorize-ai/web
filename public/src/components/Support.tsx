68ec6bd217a8d04ae2d5a1485f0f9bfd2a8e98fe
Ken Mueller <kenmueller0@gmail.com>
2020-02-25 10:27:40 -1000

    privacy and support

--- public/src/components/Support.tsx
+++ public/src/components/Support.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import { Heading } from 'react-bulma-components'
+
+import '../scss/Support.scss'
 
 export default () => (
-	<>Support</>
+	<div id="support">
+		<Heading>Support</Heading>
+		
+		Email us at support@memorize.ai or by post to:<br /><br />
+		
+		memorize.ai<br />
+		1717 Curtis Avenue<br />
+		Manhattan Beach, CA 90266<br />
+		United States
+	</div>
 )
