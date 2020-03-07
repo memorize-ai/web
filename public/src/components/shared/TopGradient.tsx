bc2ad904896c34ccb49b80f4589d9efca867b422
Ken Mueller <kenmueller0@gmail.com>
2020-03-07 15:21:33 -0800

    stack

--- public/src/components/shared/TopGradient.tsx
+++ public/src/components/shared/TopGradient.tsx
@@ -1,8 +1,8 @@
 import React, { HTMLAttributes } from 'react'
 
 export default ({ children, ...props }: HTMLAttributes<HTMLDivElement>) => (
-	<div {...props} className="top-gradient grid">
-		<div className="background origin-top-right" />
-		<div className="content z-10">{children}</div>
+	<div {...props} className="stack">
+		<div className="top-gradient origin-top-right" />
+		<div className="z-10">{children}</div>
 	</div>
 )
