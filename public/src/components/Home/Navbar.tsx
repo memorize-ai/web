c5ec8595b751d823cf2634755e95383f2f39d161
Ken Mueller <kenmueller0@gmail.com>
2020-03-06 23:54:59 -0800

    link

--- public/src/components/Home/Navbar.tsx
+++ public/src/components/Home/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import CapitalLogo from '../shared/CapitalLogo'
 import AppStoreDownloadButton from '../shared/AppStoreDownloadButton'
 
 export default () => (
 	<div className="navbar flex items-center">
-		<CapitalLogo />
+		<Link to="/">
+			<CapitalLogo />
+		</Link>
 		<AppStoreDownloadButton className="hidden sm:block ml-auto" />
 	</div>
 )
