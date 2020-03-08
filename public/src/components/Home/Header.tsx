7e29246d9592be6947e0fc6b2a0149a67ea58f0c
Ken Mueller <kenmueller0@gmail.com>
2020-03-07 16:49:14 -0800

    description

--- public/src/components/Home/Header.tsx
+++ public/src/components/Home/Header.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
+import AppStoreDownloadButton from '../shared/AppStoreDownloadButton'
 import Screenshot, { ScreenshotType } from '../shared/Screenshot'
 
 export default () => (
 	<div className="home header flex">
-		<div className="header-text text-white">
+		<div className="left flex flex-col text-white">
 			<h1>
 				The ultimate<br />
 				memorization app
 			</h1>
 			<h3>Truly effective flashcards</h3>
+			<p className="description text-dark-gray">
+				Our AI-powered spaced repetition algorithm learns as you learn.
+				Spend the least amount of time reviewing, but memorization has never been easier!
+			</p>
+			<div className="app-store-download-button-container flex">
+				<AppStoreDownloadButton className="app-store-download-button shadow-raise-on-hover" />
+			</div>
 		</div>
 		<Screenshot type={ScreenshotType.Cram} className="screenshot hidden ml-auto" />
 	</div>
