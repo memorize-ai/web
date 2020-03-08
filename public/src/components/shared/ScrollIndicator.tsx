e610e231f88d0523e87ad13ea3a3419177dd172d
Ken Mueller <kenmueller0@gmail.com>
2020-03-07 17:37:19 -0800

    no scroll indicator

--- public/src/components/shared/ScrollIndicator.tsx
+++
@@ -1,7 +0,0 @@
-import React, { HTMLAttributes } from 'react'
-
-export default ({ color, ...props }: HTMLAttributes<HTMLDivElement>) => (
-	<div {...props} className="scroll-indicator" style={{ borderColor: color }}>
-		<div style={{ background: color }} />
-	</div>
-)
