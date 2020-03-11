23e81d0318f610f18284722c69d5a38008c8a12d
Ken Mueller <kenmueller0@gmail.com>
2020-03-10 21:37:51 -0700

    integration images

--- public/src/components/Home/Integrations.tsx
+++ public/src/components/Home/Integrations.tsx
@@ -4,33 +4,65 @@ import { Link } from 'react-router-dom'
 import Logo, { LogoType } from '../shared/Logo'
 import ListItem from './IntegrationsListItem'
 import canvasImage from '../../images/home-integrations/canvas.png'
+import googleClassroomImage from '../../images/home-integrations/google-classroom.png'
+import schoologyImage from '../../images/home-integrations/schoology.png'
+import classdojoImage from '../../images/home-integrations/classdojo.png'
+import edmodoImage from '../../images/home-integrations/edmodo.png'
+import udemyImage from '../../images/home-integrations/udemy.png'
 
 export const listItems = [
 	'Share your deck with a simple link',
 	'Embed in your blog posts or ebooks',
 	'Unlock sections for each chapter for your students',
-	'Works with Canvas, Schoology, Google Classroom, and more!'
+	'Works with Canvas, Schoology, Google Classroom, Udemy, and more!'
 ]
 
 export const integrations = [
-	{ src: canvasImage, alt: 'Canvas' },
-	{ src: canvasImage, alt: 'Canvas' },
-	{ src: canvasImage, alt: 'Canvas' },
-	{ src: canvasImage, alt: 'Canvas' },
-	{ src: canvasImage, alt: 'Canvas' },
-	{ src: canvasImage, alt: 'Canvas' }
+	{
+		src: canvasImage,
+		alt: 'Canvas',
+		href: 'https://www.instructure.com/canvas/'
+	},
+	{
+		src: googleClassroomImage,
+		alt: 'Google Classroom',
+		href: 'https://classroom.google.com/'
+	},
+	{
+		src: schoologyImage,
+		alt: 'Schoology',
+		href: 'https://www.schoology.com/'
+	},
+	{
+		src: classdojoImage,
+		alt: 'ClassDojo',
+		href: 'https://www.classdojo.com/'
+	},
+	{
+		src: edmodoImage,
+		alt: 'edmodo',
+		href: 'https://www.edmodo.com/'
+	},
+	{
+		src: udemyImage,
+		alt: 'Udemy',
+		href: 'https://www.udemy.com/'
+	}
 ]
 
 export default () => {
-	const gridItems = integrations.map((integration, index) => ( // eslint-disable-next-line
-		<img
-			key={index}
-			className="bg-white shadow-raise-on-hover"
-			{...integration}
-			data-aos="flip-up"
-			data-aos-anchor="#home-integrations-aos-anchor"
-			data-aos-delay={index * 100}
-		/>
+	const gridItems = integrations.map(({ src, alt, href }, index) => (
+		<a rel="noopener noreferrer" target="_blank" href={href}>
+			<img
+				key={index}
+				className="bg-white shadow-sm shadow-raise-on-hover"
+				src={src}
+				alt={alt}
+				data-aos="flip-up"
+				data-aos-anchor="#home-integrations-aos-anchor"
+				data-aos-delay={index * 100}
+			/>
+		</a>
 	))
 	
 	return (
--- public/src/components/shared/Screenshot.tsx
+++ public/src/components/shared/Screenshot.tsx
@@ -1,7 +1,8 @@
 import React, { HTMLAttributes } from 'react'
 
 export enum ScreenshotType {
-	Cram = 'cram.png'
+	Cram = 'cram.png',
+	Market = 'market.png'
 }
 
 export default ({ type, ...props }: { type: ScreenshotType } & HTMLAttributes<HTMLImageElement>) => (
