648943498631a5f4b69eb5090d2007eec0b3e816
Ken Mueller <kenmueller0@gmail.com>
2020-03-19 01:47:32 -0700

    creates card

--- public/src/components/CreateCardPopUp/Editors.tsx
+++ public/src/components/CreateCardPopUp/Editors.tsx
@@ -4,6 +4,9 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 import Deck from '../../models/Deck'
 import Section from '../../models/Section'
+import Card from '../../models/Card'
+import LoadingState from '../../models/LoadingState'
+import Button from '../shared/Button'
 
 import '../../types/ckeditor5-react.d'
 import '../../types/ckeditor5-build-classic.d'
@@ -17,6 +20,25 @@ export default (
 ) => {
 	const [front, setFront] = useState(text)
 	const [back, setBack] = useState('')
+	const [publishLoadingState, setPublishLoadingState] = useState(LoadingState.None)
+	
+	const isPublishButtonLoading = publishLoadingState === LoadingState.Loading
+	const isPublishButtonDisabled = !(front && back)
+	
+	const createCard = async () => {
+		try {
+			setPublishLoadingState(LoadingState.Loading)
+			
+			await Card.create({ deck, section, front, back })
+			
+			setPublishLoadingState(LoadingState.Success)
+			window.close()
+		} catch (error) {
+			setPublishLoadingState(LoadingState.Fail)
+			console.error(error)
+			alert(error.message)
+		}
+	}
 	
 	return (
 		<div className="create-card-pop-up editors">
@@ -34,6 +56,32 @@ export default (
 					setBack(editor.getData())
 				}
 			/>
+			<div className="flex">
+				<Button
+					className={`
+						h-12
+						mx-auto
+						px-8
+						text-blue-${isPublishButtonDisabled ? 200 : 400}
+						${isPublishButtonDisabled || isPublishButtonLoading ? '' : 'hover:text-white'}
+						font-bold
+						uppercase
+						text-xl
+						border-2
+						border-blue-${isPublishButtonDisabled ? 200 : 400}
+						${isPublishButtonDisabled || isPublishButtonLoading ? '' : 'hover:bg-blue-400'}
+						rounded
+					`}
+					loaderSize="16px"
+					loaderThickness="3px"
+					loaderColor="#63b3ed"
+					loading={isPublishButtonLoading}
+					disabled={isPublishButtonDisabled}
+					onClick={createCard}
+				>
+					Create
+				</Button>
+			</div>
 		</div>
 	)
 }
