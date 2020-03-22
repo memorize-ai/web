fb2e0221a8e2d8b6745983ff0e7fed1dab05e3aa
Ken Mueller <kenmueller0@gmail.com>
2020-03-22 16:09:58 -0700

    search

--- public/src/constants.ts
+++ public/src/constants.ts
@@ -5,3 +5,7 @@ export const EXPECTS_SIGN_IN_KEY = 'expectsSignIn'
 export const APP_STORE_URL = 'https://apps.apple.com/app/id1462251805'
 
 export const MODAL_Z_INDEX = 100
+
+export const DECKS_HOST_IDENTIFIER = 'host-fig55q'
+export const DECKS_SEARCH_KEY = 'search-no3fo2msypcfjc1p14upkg1c'
+export const DECKS_ENGINE_NAME = 'memorize-ai-decks'
---
+++ public/src/models/Deck/Search.ts
@@ -0,0 +1,101 @@
+import { createClient } from '@elastic/app-search-javascript'
+
+import { DECKS_HOST_IDENTIFIER, DECKS_SEARCH_KEY, DECKS_ENGINE_NAME } from '../../constants'
+import Deck from '.'
+
+import '../../types/app-search-javascript.d'
+
+export enum DeckSortAlgorithm {
+	Relevance = 'Relevance',
+	Recommended = 'Recommended',
+	Top = 'Top',
+	Rating = 'Rating',
+	CurrentUsers = 'Popularity',
+	New = 'New',
+	RecentlyUpdated = 'Recently updated'
+}
+
+type RawSearchResultItemData = Record<string, { raw: any }>
+
+export default class Search {
+	private static client = createClient({
+		hostIdentifier: DECKS_HOST_IDENTIFIER,
+		searchKey: DECKS_SEARCH_KEY,
+		engineName: DECKS_ENGINE_NAME
+	})
+	
+	private static encodeSortAlgorithm = (
+		algorithm: DeckSortAlgorithm
+	): Record<string, string> | null => {
+		switch (algorithm) {
+			case DeckSortAlgorithm.Relevance:
+				return null
+			case DeckSortAlgorithm.Recommended:
+			case DeckSortAlgorithm.Top:
+				return { score: 'desc' }
+			case DeckSortAlgorithm.Rating:
+				return { average_rating: 'desc' }
+			case DeckSortAlgorithm.CurrentUsers:
+				return { current_user_count: 'desc' }
+			case DeckSortAlgorithm.New:
+				return { created: 'desc' }
+			case DeckSortAlgorithm.RecentlyUpdated:
+				return { updated: 'desc' }
+		}
+	}
+	
+	private static createDeckFromRawData = (data: RawSearchResultItemData) =>
+		new Deck(data.id.raw, {
+			topics: data.topics?.raw,
+			hasImage: data.has_image?.raw,
+			name: data.name?.raw,
+			subtitle: data.subtitle?.raw,
+			description: data.description?.raw,
+			numberOfViews: data.view_count?.raw,
+			numberOfUniqueViews: data.unique_view_count?.raw,
+			numberOfRatings: data.rating_count?.raw,
+			numberOf1StarRatings: data.one_star_rating_count?.raw,
+			numberOf2StarRatings: data.two_star_rating_count?.raw,
+			numberOf3StarRatings: data.three_star_rating_count?.raw,
+			numberOf4StarRatings: data.four_star_rating_count?.raw,
+			numberOf5StarRatings: data.five_star_rating_count?.raw,
+			averageRating: data.average_rating?.raw,
+			numberOfDownloads: data.download_count?.raw,
+			numberOfCards: data.card_count?.raw,
+			numberOfUnsectionedCards: data.unsectioned_card_count?.raw,
+			numberOfCurrentUsers: data.current_user_count?.raw,
+			numberOfAllTimeUsers: data.all_time_user_count?.raw,
+			numberOfFavorites: data.favorite_count?.raw,
+			creatorId: data.creator_id?.raw,
+			created: new Date(data.created?.raw),
+			lastUpdated: new Date(data.updated?.raw)
+		})
+	
+	static search = async (
+		query: string | null,
+		{ sortAlgorithm, filterForTopics }: {
+			sortAlgorithm: DeckSortAlgorithm
+			filterForTopics: string[] | null
+		}
+	) => {
+		const options: Record<string, any> = {}
+		const encodedSortAlgorithm = Search.encodeSortAlgorithm(sortAlgorithm)
+		
+		if (encodedSortAlgorithm)
+			options.sort = encodedSortAlgorithm
+		
+		if (filterForTopics)
+			options.filters = { topics: filterForTopics }
+		
+		return (await Search.client.search(query ?? '', options)).results
+			.map(({ data }: { data: RawSearchResultItemData }) =>
+				Search.createDeckFromRawData(data)
+			)
+	}
+	
+	static recommendedDecks = (interests: string[]) =>
+		Search.search(null, {
+			sortAlgorithm: DeckSortAlgorithm.Recommended,
+			filterForTopics: interests.length ? interests : null
+		})
+}
