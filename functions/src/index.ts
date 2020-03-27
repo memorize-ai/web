7ba6dfc162ce3d430c4f32cb7bde077568fbc446
Ken Mueller <kenmueller0@gmail.com>
2020-03-27 16:06:07 -0700

    export *

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,35 +8,8 @@ initializeApp({
 
 export { default as api } from './API'
 
-export {
-	userCreated,
-	userUpdated,
-	userDeleted
-} from './User/functions'
-
-export {
-	deckCreated,
-	deckUpdated,
-	deckDeleted,
-	deckUserNodeCreated,
-	deckUserNodeUpdated,
-	deckUserNodeDeleted,
-	deckDueCardCountPubSub,
-	deckDueCardCountNotificationPubSub
-} from './Deck/functions'
-
-export {
-	sectionDeleted
-} from './Section/functions'
-
-export {
-	cardCreated,
-	cardUpdated,
-	cardDeleted,
-	reviewCard,
-	getCardPrediction
-} from './Card/functions'
-
-export {
-	topicDeleted
-} from './Topic/functions'
+export * from './User/functions'
+export * from './Deck/functions'
+export * from './Section/functions'
+export * from './Card/functions'
+export * from './Topic/functions'
