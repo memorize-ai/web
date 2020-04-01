7853f510606c966dbac57e0ae4d325c9a327bf0c
Ken Mueller <kenmueller0@gmail.com>
2020-03-31 19:58:55 -0700

    div

--- public/src/components/shared/Stars/Star.tsx
+++ public/src/components/shared/Stars/Star.tsx
@@ -4,7 +4,7 @@ import star from '../../../images/icons/star.png'
 
 export default ({ fill }: { fill: number }) => (
 	<div>
-		{/* <div style={{ width: `${fill}%` }} /> */}
+		<div style={{ width: `${fill}%` }} />
 		<img src={star} alt={`Star ${fill}%`} />
 	</div>
 )
