fa49f6321cf17eab01ec7a218682371b54e221be
Ken Mueller <kenmueller0@gmail.com>
2020-04-08 20:37:42 -0700

    info

--- public/src/components/Dashboard/DeckPage/Controls.tsx
+++ public/src/components/Dashboard/DeckPage/Controls.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import moment from 'moment'
+import TimeAgo from 'javascript-time-ago'
+import enLocale from 'javascript-time-ago/locale/en'
 
 import Deck from '../../../models/Deck'
 import Control from './Control'
+import { formatNumber } from '../../../utils'
+
+TimeAgo.addLocale(enLocale)
+
+const timeAgo = new TimeAgo('en-US')
 
 export default ({ deck, hasDeck }: { deck: Deck, hasDeck: boolean }) => (
 	<div className="controls">
@@ -12,23 +19,23 @@ export default ({ deck, hasDeck }: { deck: Deck, hasDeck: boolean }) => (
 		<Control title="Info" className="info">
 			<div>
 				<p>Active users</p>
-				<p>{deck.numberOfCurrentUsers}</p>
+				<p>{formatNumber(deck.numberOfCurrentUsers)}</p>
 			</div>
 			<div>
 				<p>All-time users</p>
-				<p>{deck.numberOfAllTimeUsers}</p>
+				<p>{formatNumber(deck.numberOfAllTimeUsers)}</p>
 			</div>
 			<div>
 				<p>Favorites</p>
-				<p>{deck.numberOfFavorites}</p>
+				<p>{formatNumber(deck.numberOfFavorites)}</p>
 			</div>
 			<div>
 				<p>Last updated</p>
-				<p>{moment(deck.lastUpdated).format('MMM D, YYYY')}</p>
+				<p>{timeAgo.format(deck.lastUpdated)}</p>
 			</div>
 			<div>
 				<p>Date created</p>
-				{/* <p>{deck.created}</p> */}
+				<p>{moment(deck.created).format('MMM D, YYYY')}</p>
 			</div>
 		</Control>
 	</div>
