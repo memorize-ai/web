d52674d84fd6328f7f4f28345c150fb50b7a3065
Ken Mueller <kenmueller0@gmail.com>
2020-05-04 14:24:48 -0700

    better shadows

--- functions/src/App/index.ts
+++ functions/src/App/index.ts
@@ -1,3 +1,4 @@
+import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import * as express from 'express'
 import * as cors from 'cors'
@@ -5,6 +6,7 @@ import * as cors from 'cors'
 import { PRERENDER_TOKEN } from '../constants'
 import handleAPI from './API'
 
+const storage = admin.storage().bucket()
 const app = express()
 
 export default functions.https.onRequest(app as any)
@@ -14,6 +16,11 @@ app.use(require('prerender-node').set('prerenderToken', PRERENDER_TOKEN))
 
 handleAPI(app)
 
-app.get('*', (_, res) =>
+app.get('*', async ({ url }, res) => {
+	if (/\.(css|js|jpg|jpeg|gif|png|eot|otf|ttf|ttc|woff)$/i.test(url)) {
+		res.send(await storage.file(`${url.slice(url.lastIndexOf('/') + 1)}`).download())
+		return
+	}
+	
 	res.sendFile('/srv/public.html')
-)
+})
