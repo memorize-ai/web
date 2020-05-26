6612cb05723bbf630c618abf8a4b6ad464013a0e
Ken Mueller <kenmueller0@gmail.com>
2020-05-25 18:00:04 -0700

    features

---
+++ public/src/components/Home/Features/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import featuresImage from '../../../images/home/features.webp'
+
+import '../../../scss/components/Home/Features.scss'
+
+export default () => (
+	<div className="features">
+		<h2 className="title">
+			<strong>Everything you need</strong><br />
+			to start memorizing
+		</h2>
+		<img src={featuresImage} alt="Features" />
+	</div>
+)
--- public/src/components/Home/index.tsx
+++ public/src/components/Home/index.tsx
@@ -5,6 +5,7 @@ import Navbar from '../shared/Navbar'
 import Header from './Header'
 import SpacedRepetition from './SpacedRepetition'
 import Screenshots from './Screenshots'
+import Features from './Features'
 
 export default () => (
 	<div className="home">
@@ -14,5 +15,6 @@ export default () => (
 		</TopGradient>
 		<SpacedRepetition />
 		<Screenshots />
+		<Features />
 	</div>
 )
