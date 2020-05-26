6b674f3c437a229b710a933b6444d892a0419f56
Ken Mueller <kenmueller0@gmail.com>
2020-05-25 20:33:52 -0700

    background

--- public/src/components/Home/Classroom/index.tsx
+++ public/src/components/Home/Classroom/index.tsx
@@ -8,20 +8,23 @@ import '../../../scss/components/Home/Classroom.scss'
 
 export default () => (
 	<div className="classroom">
-		<img
-			className="diagram"
-			src={diagram}
-			alt="Classroom diagram"
-		/>
-		<article className="text">
-			<h2 className="title">
-				<strong>Teachers and Students</strong><br />
-				love memorize.ai
-			</h2>
-			<p className="how-it-works">
-				How it works
-			</p>
-			<List />
-		</article>
+		<div className="background" />
+		<div className="content">
+			<img
+				className="diagram"
+				src={diagram}
+				alt="Classroom diagram"
+			/>
+			<article className="text">
+				<h2 className="title">
+					<strong>Teachers and Students</strong><br />
+					love memorize.ai
+				</h2>
+				<p className="how-it-works">
+					How it works
+				</p>
+				<List />
+			</article>
+		</div>
 	</div>
 )
