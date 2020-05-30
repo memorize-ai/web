7cf9ba05f2adbd2c01454a85a18ee02d1bc1e829
Ken Mueller <kenmueller0@gmail.com>
2020-05-29 17:39:27 -0700

    footer layout

--- public/src/components/Home/Footer.tsx
+++ public/src/components/Home/Footer.tsx
@@ -7,19 +7,39 @@ import '../../scss/components/Home/Footer.scss'
 
 export default () => (
 	<footer className="footer">
-		<div className="top">
-			<h1 className="text">
-				Download the ultimate<br />
-				memorization app
-			</h1>
-			<AppStoreDownloadButton />
-		</div>
-		<div className="bottom">
-			<Logo type={LogoType.Capital} />
-			<p>
-				Copyright &copy; 2020 <b>memorize.ai Inc</b>.
-				All rights reserved
-			</p>
+		<div className="background" />
+		<div className="content">
+			<div className="top">
+				<h1 className="text">
+					Download the ultimate<br />
+					memorization app
+				</h1>
+				<AppStoreDownloadButton className="button" />
+			</div>
+			<div className="bottom">
+				<Logo className="logo" type={LogoType.Capital} />
+				<div className="socials">
+					<a
+						href="https://twitter.com/memorize_ai"
+						target="_blank"
+						rel="noopener noreferrer nofollow"
+					>
+						Twitter
+					</a>
+					<div className="divider" />
+					<a
+						href="https://twitter.com/fact_a_minute"
+						target="_blank"
+						rel="noopener noreferrer nofollow"
+					>
+						fact_a_minute
+					</a>
+				</div>
+				<p className="copyright">
+					Copyright &copy; 2020 <b>memorize.ai Inc</b>.
+					All rights reserved.
+				</p>
+			</div>
 		</div>
 	</footer>
 )
