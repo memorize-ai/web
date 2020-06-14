69246059ab1891c9df9a356bbe9623add80b522f
Ken Mueller <kenmueller0@gmail.com>
2020-06-14 02:43:05 -0700

    spaced repetition text

--- public/src/components/Home/SpacedRepetition.tsx
+++ public/src/components/Home/SpacedRepetition.tsx
@@ -14,10 +14,15 @@ const HomeSpacedRepetition = () => (
 		<article className="text">
 			<h2 className="title">
 				<strong>Spaced Repetition</strong><br />
-				is the future
+				with <strong>AI</strong>
 			</h2>
 			<p className="description">
-				abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc
+				Tired of long study sessions?
+				Memorization is <em>strongest</em> when timing it <strong>just right</strong>.
+				Try to recall too early and it won't stick.
+				Too late and you'll forget.
+				Struggle a little to remember and you won't forget!
+				We use artificial intelligence to get this spacing <strong>perfect</strong>.
 			</p>
 		</article>
 	</div>
