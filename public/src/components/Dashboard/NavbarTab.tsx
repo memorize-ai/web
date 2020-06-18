5472760d9ce051f3d5017d1d06235a6486febd2a
Ken Mueller <kenmueller0@gmail.com>
2020-06-17 17:04:34 -0700

    message for decks

--- public/src/components/Dashboard/NavbarTab.tsx
+++ public/src/components/Dashboard/NavbarTab.tsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 import cx from 'classnames'
 
 const DashboardNavbarTab = (
-	{ href, title, isSelected, isDisabled, children }: PropsWithChildren<{
+	{ href, title, isSelected, isDisabled, message, children }: PropsWithChildren<{
 		href: string
 		title: string
 		isSelected: boolean
 		isDisabled: boolean
+		message?: string
 	}>
 ) => (
 	<Link
@@ -16,7 +17,13 @@ const DashboardNavbarTab = (
 			selected: isSelected,
 			disabled: isDisabled
 		})}
+		onClick={event => isDisabled && event.preventDefault()}
 	>
+		<div
+			className="overlay"
+			aria-label={message}
+			data-balloon-pos="right"
+		/>
 		{children}
 		<p>{title}</p>
 	</Link>
