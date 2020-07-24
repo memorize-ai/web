cc757ca885ef69ed0eb5af3242fc9bd377732a0f
Ken Mueller <kenmueller0@gmail.com>
2020-07-24 12:23:40 -0700

    report message

--- functions/src/Email/functions/index.ts
+++ functions/src/Email/functions/index.ts
@@ -1,2 +1,3 @@
 export { default as dueCardsNotificationPubSub } from './dueCardsNotification'
 export { default as contactUser } from './contactUser'
+export { default as reportMessage } from './reportMessage'
---
+++ functions/src/Email/functions/reportMessage.ts
@@ -0,0 +1,62 @@
+import { https } from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+import User from '../../User'
+import { sendEmail, EmailTemplate, DEFAULT_FROM } from '../../Email'
+
+const { onCall, HttpsError } = https
+const firestore = admin.firestore()
+
+export default onCall(async data => {
+	if (typeof data !== 'object')
+		throw new HttpsError('invalid-argument', 'You must pass in an object')
+	
+	const {
+		from: fromId,
+		to: toId,
+		message: messageId,
+		reason
+	} = data
+	
+	if (!(
+		typeof fromId === 'string' &&
+		typeof toId === 'string' &&
+		typeof messageId === 'string' &&
+		typeof reason === 'string'
+	))
+		throw new HttpsError(
+			'invalid-argument',
+			'You must specify a "from", "to", "message", and "reason" as strings'
+		)
+	
+	const [from, to, message] = await Promise.all([
+		User.fromId(fromId),
+		User.fromId(toId),
+		firestore.doc(`messages/${messageId}`).get()
+	])
+	
+	if (!message.exists)
+		throw new HttpsError(
+			'failed-precondition',
+			'The message does not exist'
+		)
+	
+	await sendEmail({
+		template: EmailTemplate.ReportMessage,
+		to: DEFAULT_FROM,
+		replyTo: to.emailUser,
+		context: {
+			from: {
+				id: from.id,
+				...from.emailUser
+			},
+			to: {
+				id: to.id,
+				...to.emailUser
+			},
+			reason,
+			subject: message.get('subject'),
+			body: message.get('body')
+		}
+	})
+})
