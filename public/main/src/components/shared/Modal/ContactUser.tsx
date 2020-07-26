0a773192a28ae3b872bd0c3a5e86bad26603630a
Ken Mueller <kenmueller0@gmail.com>
2020-07-26 02:57:24 -0700

    cleaned ui

--- public/main/src/components/shared/Modal/ContactUser.tsx
+++ public/main/src/components/shared/Modal/ContactUser.tsx
@@ -1,13 +1,15 @@
 import React, { memo, useCallback, useState, FormEvent } from 'react'
+import { toast } from 'react-toastify'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons'
+import cx from 'classnames'
 
 import firebase from '../../../firebase'
 import User from '../../../models/User'
 import LoadingState from '../../../models/LoadingState'
 import Modal from '.'
 import Button from '../Button'
-import { sleep, handleError } from '../../../utils'
+import { sleep } from '../../../utils'
 
 import 'firebase/functions'
 
@@ -28,34 +30,43 @@ const ContactUserModal = (
 	const [loadingState, setLoadingState] = useState(LoadingState.None)
 	const [errorMessage, setErrorMessage] = useState(null as string | null)
 	
+	const [success, setSuccess] = useState(false)
 	const [subject, setSubject] = useState('')
 	const [body, setBody] = useState('')
 	
 	const isLoading = loadingState === LoadingState.Loading
-	const isDisabled = isLoading || !(user && body)
+	const isDisabled = !(user && body)
 	
 	const onSubmit = useCallback(async (event: FormEvent<HTMLFormElement>) => {
 		try {
 			event.preventDefault()
 			
-			if (isDisabled)
+			if (isLoading || isDisabled)
 				return
 			
 			setLoadingState(LoadingState.Loading)
 			setErrorMessage(null)
 			
 			await contactUser({ id: user!.id, subject, body })
+			
 			setLoadingState(LoadingState.Success)
+			setSuccess(true)
+			setSubject('')
+			setBody('')
 			
 			await sleep(500)
 			setIsShowing(false)
+			
+			await sleep(300)
+			setSuccess(false)
+			toast.success('Sent!')
 		} catch (error) {
-			handleError(error)
+			console.error(error)
 			
 			setLoadingState(LoadingState.Fail)
 			setErrorMessage(error.message)
 		}
-	}, [isDisabled, user, subject, body, setLoadingState, setErrorMessage, setIsShowing])
+	}, [isLoading, isDisabled, user, subject, body, setLoadingState, setSubject, setBody, setErrorMessage, setIsShowing])
 	
 	const onBodyRef = useCallback((element: HTMLTextAreaElement | null) => {
 		element?.[isShowing ? 'focus' : 'blur']()
@@ -101,14 +112,17 @@ const ContactUserModal = (
 				/>
 				<div className="footer">
 					<Button
-						className="submit-button"
+						className={cx('submit-button', { success })}
 						loaderSize="20px"
 						loaderThickness="4px"
 						loaderColor="white"
 						loading={isLoading}
 						disabled={isDisabled}
 					>
-						Send
+						{success
+							? <FontAwesomeIcon icon={faCheck} />
+							: 'Send'
+						}
 					</Button>
 					<p className="error-message">
 						{errorMessage}
