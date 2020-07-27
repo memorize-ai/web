7736100cf1411c07117655fdb7aea454a18a3a79
Ken Mueller <kenmueller0@gmail.com>
2020-07-27 01:47:13 -0700

    button styles

--- public/main/src/components/shared/ImportDeckButtons.tsx
+++ public/main/src/components/shared/ImportDeckButtons.tsx
@@ -1,11 +1,23 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileAlt, faLink } from '@fortawesome/free-solid-svg-icons'
 
 import '../../scss/components/ImportDeckButtons.scss'
 
 const ImportDeckButtons = () => {
+	const [isPasteTextModalShowing, setIsPasteTextModalShowing] = useState(false)
+	const [isPasteQuizletUrlModalShowing, setIsPasteQuizletUrlModalShowing] = useState(false)
+	
 	return (
 		<div className="import-deck-buttons">
-			
+			<button onClick={() => setIsPasteTextModalShowing(true)}>
+				<FontAwesomeIcon icon={faFileAlt} />
+				<p>Import</p>
+			</button>
+			<button onClick={() => setIsPasteQuizletUrlModalShowing(true)}>
+				<FontAwesomeIcon icon={faLink} />
+				<p>Import from Quizlet</p>
+			</button>
 		</div>
 	)
 }
