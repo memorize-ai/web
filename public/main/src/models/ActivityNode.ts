8e4e2c246665f22b838472be46604e4a3b62b197
Ken Mueller <kenmueller0@gmail.com>
2020-08-08 21:05:35 -0700

    activity node

---
+++ public/main/src/models/ActivityNode.ts
@@ -0,0 +1,30 @@
+const MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24
+
+export const getDay = () =>
+	Math.floor(Date.now() / MILLISECONDS_IN_DAY)
+
+export const getCurrentCount = () =>
+	new Date().getDay() + 1
+
+export const getFirstVisibleDay = () =>
+	getDay() - ActivityNode.PAST_COUNT - getCurrentCount()
+
+export default class ActivityNode {
+	static PAST_COUNT = 52 * 4
+	
+	static isObserving = false
+	
+	date: Date
+	
+	constructor(public day: number, public value: number) {
+		this.date = new Date(day * MILLISECONDS_IN_DAY)
+	}
+	
+	static fromSnapshot = (snapshot: firebase.firestore.DocumentSnapshot) =>
+		new ActivityNode(snapshot.get('day') ?? 0, snapshot.get('value') ?? 0)
+	
+	updateFromSnapshot = (snapshot: firebase.firestore.DocumentSnapshot) => {
+		this.value = snapshot.get('value')
+		return this
+	}
+}
