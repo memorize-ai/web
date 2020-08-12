b12074966c14b7de45235dd730079f8890b05e65
Ken Mueller <kenmueller0@gmail.com>
2020-08-12 00:21:19 -0700

    images

--- public/blog/next.config.js
+++ public/blog/next.config.js
@@ -1,3 +1,6 @@
-const withMDX = require('@next/mdx')()
+const compose = require('next-compose')
 
-module.exports = withMDX()
+module.exports = compose([
+	[require('next-images')],
+	[require('@next/mdx')()]
+])
