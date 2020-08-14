6570542d716429c0e05c2fa64901e628049bd9ce
Ken Mueller <kenmueller0@gmail.com>
2020-08-14 03:26:28 -0700

    pwa

--- public/blog/next.config.js
+++ public/blog/next.config.js
@@ -1,6 +1,13 @@
-const compose = require('next-compose')
+const compose = require('next-compose-plugins')
 
 module.exports = compose([
 	[require('next-images')],
-	[require('@next/mdx')()]
+	[require('@next/mdx')()],
+	[require('next-pwa'), {
+		pwa: {
+			disable: process.env.NODE_ENV === 'development',
+			dest: 'public',
+			runtimeCaching: require('next-pwa/cache')
+		}
+	}]
 ])
