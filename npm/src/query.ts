0e510950f9278d691b113d9cabf252b4195034be
Ken Mueller <kenmueller0@gmail.com>
2020-09-09 02:01:53 -0700

    fixed query builder

--- npm/src/query.ts
+++ npm/src/query.ts
@@ -4,5 +4,5 @@ export default (query: Query) =>
 	Object.entries(query).reduce((acc, [key, value]) => (
 		value === null || value === undefined
 			? acc
-			: `${acc ? '&' : '?'}${key}=${value}`
+			: `${acc ? `${acc}&` : '?'}${key}=${value}`
 	), '')
