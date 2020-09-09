b28faa11250201c1fc265e8ca47cc1594b3a1065
Ken Mueller <kenmueller0@gmail.com>
2020-09-08 17:56:31 -0700

    transform to use api key

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,58 +1,44 @@
-import axios from 'axios'
+import { Query } from './query'
+import fetch from './fetch'
 
-import type { MemorizeErrorStatus } from '../types'
-
-export const url = (path: string) =>
-	`https://memorize.ai/_api/${path}`
-
-export const fetch = (path: string) =>
-	axios.get(url(path))
-		.then(({ data }) => data)
-		.catch(({ response: { data, status, statusText } }) => {
-			throw new MemorizeError(
-				{ code: status, message: statusText },
-				data
-			)
-		})
-
-export const userFromId = (id: string) =>
-	fetch(`user?id=${id}`)
-
-export const deckFromId = (id: string) =>
-	fetch(`deck?id=${id}`)
-
-export const deckFromShortId = (shortId: string) =>
-	fetch(`deck?short_id=${shortId}`)
-
-export const sectionFromId = (deckId: string, sectionId: string) =>
-	fetch(`section?deck_id=${deckId}&section_id=${sectionId}`)
-
-export const sectionsFromDeck = (deckId: string, limit?: number | null) =>
-	fetch(`section?deck_id=${deckId}${limit ? `&limit=${limit}` : ''}`)
-
-export const cardFromId = (deckId: string, cardId: string) =>
-	fetch(`card?deck_id=${deckId}&card_id=${cardId}`)
-
-export const cardsFromDeck = (deckId: string, limit?: number | null) =>
-	fetch(`card?deck_id=${deckId}${limit ? `&limit=${limit}` : ''}`)
-
-export const cardsFromSection = (deckId: string, sectionId: string, limit?: number | null) =>
-	fetch(`card?deck_id=${deckId}&section_id=${sectionId}${limit ? `&limit=${limit}` : ''}`)
-
-export const topicFromId = (id: string) =>
-	fetch(`topic?id=${id}`)
-
-export const topicFromName = (name: string) =>
-	fetch(`topic?name=${name}`)
-
-export const topics = (category?: string | null) =>
-	fetch(`topic${category ? `?category=${category}` : ''}`)
-
-export class MemorizeError extends Error {
-	constructor(
-		public status: MemorizeErrorStatus,
-		public message: string
-	) {
-		super(message)
-	}
+export default class Memorize {
+	constructor(private apiKey: string) {}
+	
+	private fetch = (path: string, query: Query) =>
+		fetch(path, { key: this.apiKey, ...query })
+	
+	userFromId = (id: string) =>
+		this.fetch('user', { id })
+	
+	deckFromId = (id: string) =>
+		this.fetch('deck', { id })
+	
+	deckFromShortId = (shortId: string) =>
+		this.fetch('deck', { short_id: shortId })
+	
+	sectionFromId = (deckId: string, sectionId: string) =>
+		this.fetch('section', { deck_id: deckId, section_id: sectionId })
+	
+	sectionsFromDeck = (deckId: string, limit?: number | null) =>
+		this.fetch('section', { deck_id: deckId, limit })
+	
+	cardFromId = (deckId: string, cardId: string) =>
+		this.fetch('card', { deck_id: deckId, card_id: cardId })
+	
+	cardsFromDeck = (deckId: string, limit?: number | null) =>
+		this.fetch('card', { deck_id: deckId, limit })
+	
+	cardsFromSection = (deckId: string, sectionId: string, limit?: number | null) =>
+		this.fetch('card', { deck_id: deckId, section_id: sectionId, limit })
+	
+	topicFromId = (id: string) =>
+		this.fetch('topic', { id })
+	
+	topicFromName = (name: string) =>
+		this.fetch('topic', { name })
+	
+	topics = (category?: string | null) =>
+		this.fetch('topic', { category })
 }
+
+export { default as MemorizeError } from './error'
