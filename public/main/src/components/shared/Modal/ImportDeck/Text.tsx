b331d2d2c8e238d6150e002ea492d4a33371beb7
Ken Mueller <kenmueller0@gmail.com>
2020-09-10 18:13:24 -0700

    text modal

--- public/main/src/components/shared/Modal/ImportDeck/Text.tsx
+++ public/main/src/components/shared/Modal/ImportDeck/Text.tsx
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, { useCallback, useState, ChangeEvent } from 'react'
 
-import Modal, { ModalShowingProps } from '..'
+import { ModalShowingProps } from '..'
+import Modal from '.'
 
-const ImportDeckTextModal = ({ isShowing, setIsShowing }: ModalShowingProps) => {
+export interface ImportDeckTextModalCard {
+	front: string
+	back: string
+}
+
+export interface ImportDeckQuizletUrlModalProps extends ModalShowingProps {
+	onSubmit: (cards: ImportDeckTextModalCard[]) => void
+}
+
+const ImportDeckQuizletUrlModal = ({
+	onSubmit: _onSubmit,
+	isShowing,
+	setIsShowing
+}: ImportDeckQuizletUrlModalProps) => {
+	const [text, setText] = useState('')
+	const [cardDelimiter, setCardDelimiter] = useState('\n')
+	const [sideDelimiter, setSideDelimiter] = useState('\t')
+	
+	const onSubmit = useCallback(async () => {
+		try {
+			_onSubmit(
+				text
+					.split(cardDelimiter)
+					.reduce((acc, text) => {
+						const [front, back] = text.split(sideDelimiter)
+						
+						return front && back
+							? [...acc, { front, back }]
+							: acc
+					}, [] as ImportDeckTextModalCard[])
+			)
+		} catch (error) {
+			console.log(error)
+		}
+	}, [text, cardDelimiter, sideDelimiter, _onSubmit])
+	
+	const onTextInputChange = useCallback((event: ChangeEvent<HTMLTextAreaElement>) => {
+		setText(event.target.value)
+	}, [setText])
+	
 	return (
 		<Modal
-			isLazy={false}
+			title="Import from Quizlet"
+			onSubmit={onSubmit}
 			isShowing={isShowing}
 			setIsShowing={setIsShowing}
 		>
-			
+			<textarea
+				placeholder="Quizlet set URL"
+				value={text}
+				onChange={onTextInputChange}
+			/>
 		</Modal>
 	)
 }
 
-export default ImportDeckTextModal
+export default ImportDeckQuizletUrlModal
