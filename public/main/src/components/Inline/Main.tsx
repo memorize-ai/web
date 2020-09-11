e7696713a78db28c78998869a10f1a9318d41a97
Ken Mueller <kenmueller0@gmail.com>
2020-09-11 02:14:49 -0700

    main cards

---
+++ public/main/src/components/Inline/Main.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import cx from 'classnames'
+
+import Deck from '../../models/Deck'
+import PerformanceRating from '../../models/PerformanceRating'
+
+import styles from '../../scss/components/Inline/Main.module.scss'
+
+export interface InlineMainProps {
+	deck: Deck | null
+	rate: (rating: PerformanceRating) => void
+}
+
+const InlineMain = ({ deck, rate }: InlineMainProps) => {
+	return (
+		<main className={styles.root}>
+			<div className={styles.cards}>
+				<div className={styles.card} />
+				<div className={cx(styles.card, styles.backgroundCard_1)} />
+				<div className={cx(styles.card, styles.backgroundCard_2)} />
+			</div>
+			<footer>
+				<button>
+					Rate
+				</button>
+			</footer>
+		</main>
+	)
+}
+
+export default InlineMain
