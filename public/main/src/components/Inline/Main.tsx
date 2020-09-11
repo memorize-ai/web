36ce1ebbd562d48801dace504b7d18fb4ddba0fc
Ken Mueller <kenmueller0@gmail.com>
2020-09-11 16:01:01 -0700

    wait for rating

--- public/main/src/components/Inline/Main.tsx
+++ public/main/src/components/Inline/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import cx from 'classnames'
 
 import Deck from '../../models/Deck'
@@ -12,17 +12,33 @@ export interface InlineMainProps {
 }
 
 const InlineMain = ({ deck, rate }: InlineMainProps) => {
+	const [isWaitingForRating, setIsWaitingForRating] = useState(false)
+	
+	const waitForRating = useCallback(() => {
+		setIsWaitingForRating(true)
+	}, [setIsWaitingForRating])
+	
 	return (
-		<main className={styles.root}>
+		<main
+			className={cx(styles.root, {
+				[styles.waitingForRating]: isWaitingForRating
+			})}
+			onClick={waitForRating}
+		>
 			<div className={styles.cards}>
 				<div className={styles.card} />
 				<div className={cx(styles.card, styles.backgroundCard_1)} />
 				<div className={cx(styles.card, styles.backgroundCard_2)} />
 			</div>
 			<footer className={styles.footer}>
-				<p className={styles.waitingForRating}>
+				<p className={styles.waitForRating}>
 					Tap anywhere to continue
 				</p>
+				<div className={styles.rateButtons}>
+					<button>Easy</button>
+					<button>Struggled</button>
+					<button>Forgot</button>
+				</div>
 			</footer>
 		</main>
 	)
