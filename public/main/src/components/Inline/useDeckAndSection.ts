dd3475325b646da9e0d7f95a0f93c38c2a93eabc
Ken Mueller <kenmueller0@gmail.com>
2020-09-12 21:47:05 -0700

    use deck and section

---
+++ public/main/src/components/Inline/useDeckAndSection.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import firebase from '../../firebase'
+import Deck from '../../models/Deck'
+import { handleError } from '../../utils'
+
+import 'firebase/firestore'
+
+const firestore = firebase.firestore()
+
+export default () => {
+	const { deckId, sectionId } = useParams()
+	const [deck, setDeck] = useState(null as Deck | null)
+	
+	useEffect(() => (
+		firestore.doc(`decks/${deckId}`).onSnapshot(
+			snapshot => setDeck(Deck.fromSnapshot(snapshot, null)),
+			handleError
+		)
+	), [deckId, setDeck])
+	
+	return { deck, sectionId }
+}
