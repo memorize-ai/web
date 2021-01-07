fa4e45ca7990777ebc5469bb9a9e94e547783c91
Ken Mueller <kenmueller0@gmail.com>
2021-01-06 16:49:59 -0800

    sorts sections

--- lib/getSections.ts
+++ lib/getSections.ts
@@ -7,5 +7,6 @@ const getSections = async (id: string) =>
 	(await firestore.collection(`decks/${id}/sections`).get())
 		.docs
 		.map(Section.dataFromSnapshot)
+		.sort((a, b) => a.index - b.index)
 
 export default getSections
