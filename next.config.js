460037cd0f32c89faa81c017a938227fd877f730
Ken Mueller <kenmueller0@gmail.com>
2021-01-06 16:05:31 -0800

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = require('next-optimized-images')({
+	rewrites: () => [
+		{ source: '/sitemap.xml', destination: '/api/sitemap' }
+	],
 	redirects: () => [
 		{ source: '/d', destination: '/market', permanent: true }
 	]
