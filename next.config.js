0341972fb9448af238eca0dff440b5cb37dae469
Ken Mueller <kenmueller0@gmail.com>
2021-01-06 21:56:52 -0800

    rewrite to api url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ module.exports = require('next-optimized-images')({
 		{ source: '/sitemap.xml', destination: '/api/sitemap' },
 		{ source: '/_api/upload-deck-asset', destination: '/api/uploadDeckAsset' },
 		{ source: '/print/:slugId/:slug', destination: '/api/print/:slugId/:slug' },
-		{ source: '/print/:slugId/:slug/s/:sectionId', destination: '/api/print/:slugId/:slug/:sectionId' }
+		{ source: '/print/:slugId/:slug/s/:sectionId', destination: '/api/print/:slugId/:slug/:sectionId' },
+		{ source: '/api/:method', destination: 'https://us-central1-memorize-ai.cloudfunctions.net/api/:method' }
 	],
 	redirects: () => [
 		{ source: '/d', destination: '/market', permanent: true }
