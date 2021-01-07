3f97778407923bffdeffd72fef4f2ced74ddcb48
Ken Mueller <kenmueller0@gmail.com>
2021-01-06 16:46:12 -0800

    preloads only 1k decks

--- lib/getDecks.ts
+++ lib/getDecks.ts
@@ -3,9 +3,15 @@ import firebase from './firebase/admin'
 
 const firestore = firebase.firestore()
 
-const getDecks = async () =>
-	(await firestore.collection('decks').get())
-		.docs
-		.map(Deck.dataFromSnapshot)
+const getQuery = (limit: number | null) => {
+	const collection = firestore.collection('decks')
+	
+	return limit === null
+		? collection
+		: collection.orderBy('currentUserCount', 'desc').limit(limit)
+}
+
+const getDecks = async (limit: number | null = null) =>
+	(await getQuery(limit).get()).docs.map(Deck.dataFromSnapshot)
 
 export default getDecks
