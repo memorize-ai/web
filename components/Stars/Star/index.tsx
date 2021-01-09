f8f36a5efe25b3e913b2e51e56b5c15361081baa
kenmueller <kenmueller0@gmail.com>
2021-01-08 22:02:45 -0800

    stars styles

--- components/Stars/Star/index.tsx
+++ components/Stars/Star/index.tsx
@@ -1,10 +1,11 @@
 import Img from 'react-optimized-image'
 
 import star from 'images/icons/star.jpg'
+import styles from './index.module.scss'
 
 const Star = ({ fill }: { fill: number }) => (
-	<div>
-		<div style={{ width: `${fill}%` }} />
+	<div className={styles.root}>
+		<div className={styles.background} style={{ width: `${fill}%` }} />
 		<Img src={star} alt={`Star ${fill}%`} webp original />
 	</div>
 )
