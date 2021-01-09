3c492f4817b810d6f9650cb8d3db435e0f1688a8
kenmueller <kenmueller0@gmail.com>
2021-01-08 21:56:32 -0800

    text area styles

--- components/TextArea/index.tsx
+++ components/TextArea/index.tsx
@@ -1,6 +1,17 @@
 import { TextareaHTMLAttributes } from 'react'
 import cx from 'classnames'
 
+import styles from './index.module.scss'
+
+export interface TextAreaProps
+	extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+	className?: string
+	minHeight?: string | number
+	placeholder?: string
+	value: string
+	setValue: (value: string) => void
+}
+
 const TextArea = ({
 	className,
 	minHeight,
@@ -8,16 +19,10 @@ const TextArea = ({
 	value,
 	setValue,
 	...props
-}: {
-	className?: string
-	minHeight?: string | number
-	placeholder?: string
-	value: string
-	setValue: (value: string) => void
-} & TextareaHTMLAttributes<HTMLTextAreaElement>) => (
+}: TextAreaProps) => (
 	<textarea
 		{...props}
-		className={cx('textarea', className)}
+		className={cx(styles.root, className)}
 		style={{ minHeight }}
 		placeholder={placeholder}
 		value={value}
