01ed78efc3729fd933b0fbb89a3174571e3ee50b
kenmueller <kenmueller0@gmail.com>
2021-01-18 16:44:46 -0800

    account settings profile

--- components/Dashboard/Settings/Account/index.tsx
+++ components/Dashboard/Settings/Account/index.tsx
@@ -1,10 +1,12 @@
 import Settings from '..'
+import Profile from './Profile'
 import Name from './Name'
 import ForgotPassword from './ForgotPassword'
 import SignOut from './SignOut'
 
 const AccountSettings = () => (
 	<Settings title="Account" description="Edit your account">
+		<Profile />
 		<Name />
 		<ForgotPassword />
 		<SignOut />
